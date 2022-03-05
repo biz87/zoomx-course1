ae99d1190d561a41e051e4e458e771729a32a732
Nikolay Savin <info@megawebs.kz>
2022-03-06 01:11:29 +0600

    мелкие правки

--- core/components/zoomx/src/Controllers/Api/Pages/GetController.php
+++ core/components/zoomx/src/Controllers/Api/Pages/GetController.php
@@ -2,13 +2,11 @@
 
 namespace Zoomx\Controllers\Api\Pages;
 
-use modResource;
-
 class GetController extends BaseController
 {
     public function index($id)
     {
-        $page = $this->modx->getObject(modResource::class, ['id' => $id, 'deleted' => 0]);
+        $page = $this->modx->getObject(\modResource::class, ['id' => $id, 'deleted' => 0]);
         if ($page) {
             return jsonx([
                 'id' => $page->id,
--- core/config/routes.php
+++ core/config/routes.php
@@ -3,7 +3,7 @@
 /** @var FastRoute\RouteCollector  $router */
 /** @var modX  $modx */
 
-$router->get('api/pages/{id}', [Zoomx\Controllers\Api\Pages\GetController::class]);
-$router->post('api/pages/', [Zoomx\Controllers\Api\Pages\CreateController::class]);
-$router->put('api/pages/{id}', [Zoomx\Controllers\Api\Pages\UpdateController::class]);
-$router->delete('api/pages/{id}', [Zoomx\Controllers\Api\Pages\DeleteController::class]);
+$router->get('api/pages/{id}', Zoomx\Controllers\Api\Pages\GetController::class);
+$router->post('api/pages/', Zoomx\Controllers\Api\Pages\CreateController::class);
+$router->put('api/pages/{id}', Zoomx\Controllers\Api\Pages\UpdateController::class);
+$router->delete('api/pages/{id}', Zoomx\Controllers\Api\Pages\DeleteController::class);
